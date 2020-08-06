4b31f4b9758775a6919e10b220051cbc9fb094ac
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-06 00:28:11 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,6 +22,8 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
+  epic_tragedy[:montague][:hero][:status]="dead"
+  epic_tragedy[:capulet][:hero][:status]="dead"
 
 
 
@@ -59,6 +61,7 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
+   
 
 
 
